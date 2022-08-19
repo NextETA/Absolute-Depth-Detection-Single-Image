99bf1468dc2ac9a17b2217888bd659467e2bd962
NextETA <157932052+NextETA@users.noreply.github.com>
2022-08-19 18:41:26 +0000

    Release v0.6.0

--- estimateDepth.py
+++ estimateDepth.py
@@ -30,3 +30,35 @@ def cnn_model_fn(features, labels, mode):
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
+
+  # Pooling Layer #1
+  # First max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 480, 640, 8]
+  # Output Tensor Shape: [batch_size, 240, 320, 8]
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[4, 4], strides=4)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 240, 320, 8]
+  # Output Tensor Shape: [batch_size, 240, 320, 16]
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=4,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #2
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 240, 320, 16]
+  # Output Tensor Shape: [batch_size, 120, 160, 16]
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+  # Flatten tensor into a batch of vectors
+  # Input Tensor Shape: [batch_size, 120, 160, 16]
+  # Output Tensor Shape: [batch_size, 120 * 160 * 16]
+  pool2_flat = tf.reshape(pool2, [-1, 60 * 80 * 4])
+
+  # Dense Layer
+  # Densely connected layer with 1024 neurons

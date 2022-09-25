a78b52e01c270d056b27066144787b10c54355fd
NextETA <157932052+NextETA@users.noreply.github.com>
2022-09-25 08:12:12 +0000

    ROBE-34

--- estimateSize.py
+++ estimateSize.py
@@ -88,3 +88,22 @@ def estimateSize():
     # array to hold (img#, bb#, null, null, x, y, h, w, d, img_h, img_w)
     imageUnLabeled = np.zeros((n, 11))
     imageUnLabeled[:, 0:2] = unlabeledWithDescription
+
+    # Part 6: Create bounding boxes for our testing images
+    for i in range(n):
+        imgNum = int(imageUnLabeled[i, 0])
+        imgi = images[:, :, :, imgNum]
+        h, w, c = imgi.shape
+
+        # show the image
+        # pilimg = Image.fromarray(imgi, 'RGB')
+        # pilimg.show()
+
+        # bbox size [k,5] where n is image number, k is num of objects in each image
+        # last dimension has x, y, height, width, depth of each bbox in image i
+        bbox = find_BB_and_depth(imgi, depths[:, :, imgNum], False)
+
+        # get the bouning box number
+        k = int(imageUnLabeled[i, 1])
+
+        # add the bbox values to the imageLabel

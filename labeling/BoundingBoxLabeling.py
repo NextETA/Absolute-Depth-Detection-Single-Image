71874baedbf8beae0e483fc6ac5683e146dd894a
NextETA <157932052+NextETA@users.noreply.github.com>
2022-12-15 14:20:42 +0000

    sched/debug: Make sysrq prints of sched debug data optional

--- labeling/BoundingBoxLabeling.py
+++ labeling/BoundingBoxLabeling.py
@@ -8,4 +8,4 @@ def BoundingBoxLabeling(img_rgb, pixel_depths, drawContours = False, imageNum = 
     img = cv2.pyrDown(img_rgb)
 
     ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
-    image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_T
+    image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SI

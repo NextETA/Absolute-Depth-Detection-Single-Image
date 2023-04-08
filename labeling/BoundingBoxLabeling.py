73934678e81d3234676e099de03770d85f24eca7
NextETA <157932052+NextETA@users.noreply.github.com>
2023-04-08 05:36:43 +0000

    Linux 3.4.43

--- labeling/BoundingBoxLabeling.py
+++ labeling/BoundingBoxLabeling.py
@@ -32,4 +32,4 @@ def BoundingBoxLabeling(img_rgb, pixel_depths, drawContours = False, imageNum = 
     for i in listOfBBs:
         img = cv2.pyrDown(img_rgb)
 
-        ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255
+        ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_

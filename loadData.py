312cbaf21add37e33ffb659f50beafc001af237f
NextETA <157932052+NextETA@users.noreply.github.com>
2023-06-23 19:15:24 +0000

    Add podspec file

--- loadData.py
+++ loadData.py
@@ -20,3 +20,20 @@ for i in range (0,len(imgs)):
     imgi = images[i,:,:,:]
     # bbox size [k,5] where n is image number, k is num of objects in each image
     # last dimension has x, y, height, width, depth of each bbox in image i
+    bbox = find_BB_and_depth(imgi)
+    # add to the allBBoxes matrix
+    np.cat((allBBoxes,bbox),axis=3)
+
+# get the labeled height and widths of k objects in n images
+# size = n x k x 2 where last dimension is height and width in meters
+labels = [];
+
+# do training on linear regression
+model = LinearRegression(regLambda = 0.00001)
+model.fit(X, y)
+
+# predict sizes for k objects in n images using linreg
+
+# do training on neural nets
+
+# predict sizes for k objects in n images using neuralnets

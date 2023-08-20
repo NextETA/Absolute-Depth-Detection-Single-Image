f2b8b258dd8a2a92ba0a5c1ae77b9535a830a45b
NextETA <157932052+NextETA@users.noreply.github.com>
2023-08-20 20:05:24 +0000

    Manual reversion of r583 and r570 - the removal of the svn:mime-type property from all files.

--- testing/LabelingRig.py
+++ testing/LabelingRig.py
@@ -2,4 +2,5 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import numpy as np
 import cv2
-from BoundingBoxLabeling import BoundingBoxLabelin
+from BoundingBoxLabeling import BoundingBoxLabeling
+from linreg_closedform

f6e7c0bda32a79d969a6a56d575329f4abc654a8
NextETA <157932052+NextETA@users.noreply.github.com>
2023-09-15 17:30:55 +0000

    Added gulpfile

--- testing/LabelingRig.py
+++ testing/LabelingRig.py
@@ -3,4 +3,5 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import numpy as np
 import cv2
 from BoundingBoxLabeling import BoundingBoxLabeling
-from linreg_closedform import LinearRegressionClosedForm as Linea
+from linreg_closedform import LinearRegressionClosedForm as LinearRegression
+from

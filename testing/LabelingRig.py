2fdef6684b170cfd7bb658e0ad8f6929d5d5cdf1
NextETA <157932052+NextETA@users.noreply.github.com>
2023-10-30 15:13:09 +0000

    font color

--- testing/LabelingRig.py
+++ testing/LabelingRig.py
@@ -21,4 +21,4 @@ while(1):
         # bbox size [k,5] where n is image number, k is num of objects in each image
         # last dimension has x, y, height, width, depth of each bbox in image i
         bbox = BoundingBoxLabeling(imgi, depths[:,:,i], True, imageNum = i)
-        entered 
+        entered = raw_input("See this i

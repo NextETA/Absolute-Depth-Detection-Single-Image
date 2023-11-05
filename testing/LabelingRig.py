632074a1e79362271973199fb98a2bfee33ad020
NextETA <157932052+NextETA@users.noreply.github.com>
2023-11-05 16:14:55 +0000

    typo in comments

--- testing/LabelingRig.py
+++ testing/LabelingRig.py
@@ -23,4 +23,5 @@ while(1):
         bbox = BoundingBoxLabeling(imgi, depths[:,:,i], True, imageNum = i)
         entered = raw_input("See this image again (n/y): ")
         if entered != 'y':
-            bre
+            break
+        pilimg =

bd6668eea8fea94f18b4b07eaf60654e5383de10
NextETA <157932052+NextETA@users.noreply.github.com>
2023-11-16 20:50:30 +0000

    Fix copyright year

--- testing/testingRig_UNTESTED.py
+++ testing/testingRig_UNTESTED.py
@@ -13,3 +13,20 @@ import sys
 def estimateSize():
 
     # Part 0: Loading the data with depth from matlab to python
+
+    # convert the matlab file to python
+    # load_mat_to_python()
+
+    # Part 1: Loading image and associated depth data into python
+
+    # load all the data
+    depths = np.load('data/nyu_dataset_depths.npy')
+    images = np.load('data/nyu_dataset_images.npy')
+    # labels = np.load('nyu_dataset_labels.npy')
+    # names = np.load('nyu_dataset_names.npy')
+    # scenes = np.load('nyu_dataset_scenes.npy')
+
+    # Part 2: Import labels n by 4 (img #, bb#, lab_h, lab_w)
+
+    labels = np.loadtxt('data/ImageLabels.dat', delimiter=',')
+    n, d = labels.shape
